c11ffa05d844653ebfb5e7790de7a5a02db273b4
[nick_fasulo] <fasulo.nick@gmail.com>
2022-01-11 19:40:29 -0500

    removed comments from index.js

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const { chromium } = require('playwright');
     headless: false,
   });
 
-  // YOUR CODE STARTS
   const page = await browser.newPage();
   await page.goto('https://www.netflix.com');
   await page.click('text=Sign In');
   await page.fill('#id_userLoginId', 'username');
   await page.fill('#id_password', 'password');
   await page.click('button:has-text("Sign In")');
-  // YOUR CODE ENDS
 })();
